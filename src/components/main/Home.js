a90f0e779dfc7740380a30610607fee1eb3bdc71
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-26 17:19:48 -0500

    Pseudo-coded for products per row

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -15,6 +15,21 @@ export default function Home() {
           src='https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg' 
           alt='Hero'
         />
+
+        <div className='home-row'>
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className='home-row'>
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+        
+        <div className='home-row'>
+          {/* Product */}
+        </div>
       </div>
     </div>
   )
