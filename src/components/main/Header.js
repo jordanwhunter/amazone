ed09c693b6e737f0934b201858ec31749e232a72
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-26 16:13:06 -0500

    Building Header

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -7,11 +7,38 @@ export default function Header() {
   return (
     <div className='header'>
       {/* Logo */}
-      <img src={amazone} alt='Amazone'/>
-      
+      <img 
+        className='header-logo'
+        src={amazone} 
+        alt='Amazone'
+      />
+
       {/* Search box */}
+      <div className='header-search'>
+        <input 
+          className='header-search-input'
+          type='text'
+        />
+      </div>
 
       {/* Children */}
+      <div className='header-nav'>
+        <div className='header-option'>
+          {/* Hello / Sign In */}
+          <span className='header-option-line-one'>Hello</span>
+          <span className='header-option-line-two'>Sign in</span>
+        </div>
+        <div className='header-option'>
+          {/* Returns & Orders */}
+          <span className='header-option-line-one'>Returns</span>
+          <span className='header-option-line-two'>& Orders</span>
+        </div>
+        <div className='header-option'>
+          {/* Your Prime */}
+          <span className='header-option-line-one'>Your</span>
+          <span className='header-option-line-two'>Prime</span>
+        </div>
+      </div>
     </div>
   )
 };
