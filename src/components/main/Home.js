df2d0ff6c1cf516632a6ca3c28a0d1559b7e934d
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-26 17:14:01 -0500

    Imported hero image via src

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -8,8 +8,14 @@ import '../../styles/main/Home.css';
 
 export default function Home() {
   return (
-    <div>
-      Hello world. I am the home component.
+    <div className='home'>
+      <div className='home-container'>
+        <img
+          className='home-image'
+          src='https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg' 
+          alt='Hero'
+        />
+      </div>
     </div>
   )
 };
