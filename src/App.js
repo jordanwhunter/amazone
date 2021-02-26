8bf3673340adfccdeae3fae98978a7809ce2995a
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-26 16:12:45 -0500

    Imported Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/main/Header';
 
 function App() {
   return (
     <div className='app'>
-      <h1>Hello World! 👋</h1>
       {/* Header */}
+      <Header />
 
       {/* Home */}
     </div>
