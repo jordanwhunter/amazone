41cd598a68bb34cb8b1b9c37bb299cd6d51dfbc7
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-26 17:23:38 -0500

    Created Product component

---
+++ src/components/main/Product.js
@@ -0,0 +1,14 @@
+// Dependencies
+import React from 'react';
+
+// Icons
+
+// Styles
+
+export default function Product() {
+  return (
+    <div>
+      Hello world. I am the product component.
+    </div>
+  )
+};
