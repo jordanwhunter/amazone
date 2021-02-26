1a300cab90a941e3e50143073bb0b63ee5c48a7d
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-26 16:59:02 -0500

    Created Home.js component

---
+++ src/components/main/Home.js
@@ -0,0 +1,14 @@
+// Dependencies
+import React from 'react';
+
+// Icons
+
+// Styles
+
+export default function Home() {
+  return (
+    <div>
+      Hello world. I am the home component.
+    </div>
+  )
+};
