f549403d2d5a99ed01b07a752b59e2df787a0cb2
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-26 16:00:53 -0500

    Import image

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import amazone from '../../images/amazone.png';
 
 import '../../styles/Header.css';
 
 export default function Header() {
   return (
-    <div>
-      Hello world. I'm the header component.
+    <div className='header'>
+      {/* Logo */}
+      <img src={amazone} alt='Amazone'/>
+      
+      {/* Search box */}
+
+      {/* Children */}
     </div>
   )
 };
