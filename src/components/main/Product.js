93d9534791c287690ecc4c15e52b76ca09f848ee
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-27 12:49:36 -0500

    Created dynamic product component

--- src/components/main/Product.js
+++ src/components/main/Product.js
@@ -1,40 +1,33 @@
 // Dependencies
 import React from 'react';
-import ctci from '../../images/products/ctci.png'
 
 // Icons
-import StarIcon from '@material-ui/icons/Star';
-import StarHalfIcon from '@material-ui/icons/StarHalf';
 
 // Styles
 import '../../styles/main/Product.css';
 
-export default function Product() {
+export default function Product({ title, image, price, rating }) {
   return (
     <div className='product'>
       <div className='product-info'>
         {/* Title */}
-        <p>Cracking the Coding Interview</p>
+        <p>{title}</p>
 
         <p className='product-price'>
           {/* Price */}
-          <small>$</small><strong>23.99</strong>
+          <small>$</small><strong>{price}</strong>
         </p>
 
         <div className='product-rating'>
           {/* Star rating */}
-          <StarIcon />
-          <StarIcon />
-          <StarIcon />
-          <StarIcon />
-          <StarHalfIcon />
+          <p>{'⭐️'.repeat(rating)}</p>
         </div>
       </div>
 
       {/* Image */}
       <img 
-        src={ctci}
-        alt='Cracking the Coding Interview'
+        src={image}
+        alt={title}
       />
       {/* Add to cart button */}
       <button>Add to Cart</button>
