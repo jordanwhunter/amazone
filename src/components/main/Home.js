742a2a02a915f1caa5b7cd69909c68af28eb04f3
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-27 18:39:00 -0500

    Output products

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -4,7 +4,9 @@ import Product from './Product';
 import ctci from '../../images/products/ctci.png';
 import sideTrak from '../../images/products/sideTrack.jpg';
 import playstation from '../../images/products/playstation5.jpg';
-import breville from '../../images/products/breville-bes870xl.jpg'
+import breville from '../../images/products/breville-bes870xl.jpg';
+import airpods from '../../images/products/apple-airpods-pro.jpg';
+import samsung from '../../images/products/samgsung-cj890.jpg';
 
 // Icons
 
@@ -22,7 +24,8 @@ export default function Home() {
         />
 
         <div className='home-row'>
-          <Product 
+          <Product
+            id='12321341' 
             title='Cracking the Coding Interview - 6th Edition'
             dollars={23}
             cents={99}
@@ -30,6 +33,7 @@ export default function Home() {
             rating={4}
           />
           <Product
+            id='49538094'
             title='SideTrak Swivel Triple Portable Monitor for Laptop'
             dollars={629}
             cents={99}
@@ -40,6 +44,7 @@ export default function Home() {
 
         <div className='home-row'>
           <Product
+            id='4903850'
             title='PlayStation 5 Console'
             dollars={499}
             cents={99}
@@ -47,17 +52,32 @@ export default function Home() {
             rating={5}
           />
           <Product
+            id='23445930'
             title='Breville BES870XL Espresso Machine'
             dollars={699}
             cents={95}
             image={breville}
             rating={4}
           />
-          <Product />
+          <Product
+            id='32543543'
+            title='Apple AirPods Pro'
+            dollars={219}
+            cents={99}
+            image={airpods}
+            rating={5}
+          />
         </div>
 
         <div className='home-row'>
-          <Product />
+          <Product
+            id='90829332'
+            title='Samsung C49J890DKN 49" Super Ultra-Wide Desktop Monitor'
+            dollars={999}
+            cents={99}
+            image={samsung}
+            rating={4}
+          />
         </div>
       </div>
     </div>
