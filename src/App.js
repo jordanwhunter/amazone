4a5a465b5fa3efae9ac259268328b2b030f76ec4
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-27 19:15:08 -0500

    Imported Checkout component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/main/Header';
 import Home from './components/main/Home';
+import Checkout from './components/cart/Checkout';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
           </Route>
           <Route path='/checkout'>
             <Header />
-            <h1>Hello world! I'm testing output for the Checkout component.</h1>
+            <Checkout />
           </Route>
         </Switch> 
       </div>
