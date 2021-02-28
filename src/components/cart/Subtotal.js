0c759fab514afab3162a7833097aa7c099ac9584
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-27 19:44:50 -0500

    Created Subtotal component

---
+++ src/components/cart/Subtotal.js
@@ -0,0 +1,13 @@
+// Dependencies
+import React from 'react';
+
+// Styles
+import '../../styles/cart/Subtotal.css';
+
+export default function Subtotal() {
+  return (
+    <div className='subtotal'>
+      Hello world. I am the subtotal component
+    </div>
+  )
+};
