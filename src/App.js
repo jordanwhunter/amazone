e277d7ebef868383cd2af95ecb262d20b3398b61
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-27 19:06:38 -0500

    Testing output for checkout

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
     <Router>
       <div className='app'>
         <Switch>
+          <Route path='/checkout'>
+            <Header />
+            <h1>Hello world! I'm testing output for the Checkout component.</h1>
+          </Route>
           <Route path='/'>
             <Header />
             <Home />
