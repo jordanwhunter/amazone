5a8d7627d82ea39e07c1f13206253b7d1206fa4d
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-27 19:58:25 -0500

    Building skeleton for Subtotal component

--- src/components/cart/Subtotal.js
+++ src/components/cart/Subtotal.js
@@ -1,5 +1,6 @@
 // Dependencies
 import React from 'react';
+import CurrencyFormat from 'react-currency-format';
 
 // Styles
 import '../../styles/cart/Subtotal.css';
@@ -7,7 +8,24 @@ import '../../styles/cart/Subtotal.css';
 export default function Subtotal() {
   return (
     <div className='subtotal'>
-      Hello world. I am the subtotal component
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 items):
+              <strong>{` ${value}`}</strong>
+            </p>
+            <small className='subtotal-gift'>
+              <input type='checkbox' /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={'text'}
+        thousandSeparator={true}
+        prefix={'$'}
+      />
     </div>
   )
 };
