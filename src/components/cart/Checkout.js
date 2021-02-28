72bd2f6d8df4a69af9c0736876dfef9a35d6afeb
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-27 19:14:42 -0500

    Created Checkout component

---
+++ src/components/cart/Checkout.js
@@ -0,0 +1,13 @@
+// Dependencies
+import React from 'react';
+
+// Styles
+import '../../styles/cart/Checkout.css';
+
+export default function Checkout() {
+  return (
+    <div>
+      Hello world! I'm the Checkout component.
+    </div>
+  )
+};
