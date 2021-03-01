5d18bd1a216414dce9ff4c64862b3b622e4572ad
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-01 17:33:16 -0500

    Implemented price change and deleted dollars and cents

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -27,16 +27,14 @@ export default function Home() {
           <Product
             id='12321341' 
             title='Cracking the Coding Interview - 6th Edition'
-            dollars={23}
-            cents={99}
+            price={23.99}
             image={ctci}
             rating={4}
           />
           <Product
             id='49538094'
             title='SideTrak Swivel Triple Portable Monitor for Laptop'
-            dollars={629}
-            cents={99}
+            price={629.99}
             image={sideTrak}
             rating={5}
           />
@@ -46,24 +44,21 @@ export default function Home() {
           <Product
             id='4903850'
             title='PlayStation 5 Console'
-            dollars={499}
-            cents={99}
+            price={499.99}
             image={playstation}
             rating={5}
           />
           <Product
             id='23445930'
             title='Breville BES870XL Espresso Machine'
-            dollars={699}
-            cents={95}
+            price={699.95}
             image={breville}
             rating={4}
           />
           <Product
             id='32543543'
             title='Apple AirPods Pro'
-            dollars={219}
-            cents={99}
+            price={219.99}
             image={airpods}
             rating={5}
           />
@@ -73,8 +68,7 @@ export default function Home() {
           <Product
             id='90829332'
             title='Samsung C49J890DKN 49" Super Ultra-Wide Desktop Monitor'
-            dollars={999}
-            cents={99}
+            price={999.99}
             image={samsung}
             rating={4}
           />
