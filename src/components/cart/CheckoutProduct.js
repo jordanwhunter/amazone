bce1684e3e95cebc0ca9274562986901876e067f
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-01 17:44:13 -0500

    Created CheckoutProduct component and imported stylesheet

---
+++ src/components/cart/CheckoutProduct.js
@@ -0,0 +1,13 @@
+// Dependencies
+import React from 'react';
+
+// Styles
+import '../../styles/cart/CheckoutProduct.css';
+
+export default function CheckoutProduct() {
+  return (
+    <div className='checkout-product'>
+      Hello world. I am the CheckoutProduct component!
+    </div>
+  )
+};
