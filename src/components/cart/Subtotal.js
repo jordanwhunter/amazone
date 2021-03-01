38769141c2be868c2909dc6d7ebbdffafea5fb2e
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-01 17:43:29 -0500

    Refactored notes

--- src/components/cart/Subtotal.js
+++ src/components/cart/Subtotal.js
@@ -14,6 +14,7 @@ export default function Subtotal() {
   return (
     <div className='subtotal'>
       <CurrencyFormat
+        // value acts as a render prop
         renderText={(value) => (
           <>
             <p>
