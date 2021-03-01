9451c290c462277dba8be037e5bd8a5ceb41d2b8
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-01 16:23:48 -0500

    Implement context provider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { StateProvider } from './contexts/StateContext';
+import reducer, { initialState } from './reducer';
 import Header from './components/main/Header';
 import Home from './components/main/Home';
 import Checkout from './components/cart/Checkout';
@@ -7,17 +9,22 @@ import Checkout from './components/cart/Checkout';
 function App() {
   return (
     <Router>
-      <div className='app'>
-        <Header />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/checkout'>
-            <Checkout />
-          </Route>
-        </Switch> 
-      </div>
+      <StateProvider 
+        initialState={initialState}
+        reducer={reducer}
+      >
+        <div className='app'>
+          <Header />
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/checkout'>
+              <Checkout />
+            </Route>
+          </Switch> 
+        </div>
+      </StateProvider>
     </Router>
   );
 };
