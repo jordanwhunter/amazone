b050949269e9dce49a956ed6b741df20246ddf31
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-02 15:02:51 -0500

    Imported image and wrapped in Link

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,5 +1,7 @@
 // Dependencies
 import React from 'react';
+import { Link } from 'react-router-dom';
+import amazone from '../../images/logo/amazone.png';
 
 // Styles
 import '../../styles/authentication/Login.css';
@@ -7,7 +9,14 @@ import '../../styles/authentication/Login.css';
 export default function Login() {
   return (
     <div className='login'>
-      Hello world. I am the Login component.
+      <Link to='/'>
+        <img
+          className='login-logo'
+          src={amazone}
+          alt='Amazone Logo' 
+        />
+      </Link>
+      
     </div>
   )
 };
