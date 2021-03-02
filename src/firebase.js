063fc538f9a39f25b6848928dfae0198d648453e
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-02 16:01:50 -0500

    Exports for db and auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -8,5 +10,8 @@ const app = firebase.initializeApp({
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
+
+export const db = app.firestore();
+export const auth = app.auth();
 
 export default app;
