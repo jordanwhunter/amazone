ae498885e57d4b7103e5922610ab9b1a0c022c41
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-01 21:17:52 -0500

    Handled for REMOVE_FROM_CART in switch case

--- src/reducer.js
+++ src/reducer.js
@@ -7,7 +7,7 @@ export const getCartTotal = (cart) =>
   cart?.reduce((amount, item) => item.price + amount, 0);
 
 export default function reducer(state, action) {
-  const { type, item } = action
+  const { type, item, id } = action
   // console.log(action)
 
   switch (type) {
@@ -15,8 +15,27 @@ export default function reducer(state, action) {
       return {
         ...state,
         cart: [...state.cart, item],
-      }
+      };
+    case 'REMOVE_FROM_CART':
+      const index = state.cart.findIndex(
+        (cartItem) => cartItem.id === id
+      );
+      let newCart = [...state.cart];
+
+      if (index >= 0) {
+        newCart.splice(index, 1);
+      } else {
+        console.warn(
+          `Cannot remove product ID ${id} - does not exist in cart`
+        )
+      };
+      
+      return {
+        ...state,
+        cart: newCart
+      };
+
     default:
       return state
-  }
+  };
 };
