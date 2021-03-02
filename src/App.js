15c5b55ef927c22dab3ed63f6e45e0db02a036fa
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-02 18:30:56 -0500

    Imported AuthProvider and wrapped around all other components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { StateProvider } from './contexts/StateContext';
+import { AuthProvider } from './contexts/AuthContext';
 import reducer, { initialState } from './reducer';
 import Header from './components/main/Header';
 import Home from './components/main/Home';
 import Checkout from './components/cart/Checkout';
 import CheckoutProduct from './components/cart/CheckoutProduct';
 import Login from './components/authentication/Login';
+import Signup from './components/authentication/Signup';
 
 function App() {
   return (
     <Router>
-      <StateProvider 
-        initialState={initialState}
-        reducer={reducer}
-      >
-        <div className='app'>
-          <Switch>
-            {/* Authentication routes */}
-            <Route path='/login' component={Login} />
+      <AuthProvider>
+        <StateProvider 
+          initialState={initialState}
+          reducer={reducer}
+        >
+          <div className='app'>
+            <Switch>
+              {/* Authentication routes */}
+              <Route path='/signup' component={Signup} />
+              <Route path='/login' component={Login} />
 
-            {/* Main page route */}
-            <Route exact path='/'>
-            <Header />
-            <Home />
-            </Route>
+              {/* Main page route */}
+              <Route exact path='/'>
+                <Header />
+                <Home />
+              </Route>
 
-            {/* Checkout route */}
-            <Route path='/checkout'>
-              <Header />
-              <Checkout />
-              <CheckoutProduct />
-            </Route>
-          </Switch> 
-        </div>
-      </StateProvider>
+              {/* Checkout route */}
+              <Route path='/checkout'>
+                <Header />
+                <Checkout />
+                <CheckoutProduct />
+              </Route>
+            </Switch> 
+          </div>
+        </StateProvider>
+      </AuthProvider>
     </Router>
   );
 };
