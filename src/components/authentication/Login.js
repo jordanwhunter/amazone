4b39aba6b12ddca60529851890e7e0be0467bca8
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-02 15:31:18 -0500

    Created login-container

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,7 +1,7 @@
 // Dependencies
 import React from 'react';
 import { Link } from 'react-router-dom';
-import amazone from '../../images/logo/amazone.png';
+import amazone from '../../images/logo/amazone-bw.png';
 
 // Styles
 import '../../styles/authentication/Login.css';
@@ -17,6 +17,24 @@ export default function Login() {
         />
       </Link>
       
+      <div className='login-container'>
+        <h1>Log In</h1>
+        <form>
+          <h5>Email</h5>
+          <input type='text'/>
+
+          <h5>Password</h5>
+          <input type='password'/>
+
+          <button className='login-button'>Log In</button>
+        </form>
+
+        <p>
+          By logging in, you agree to Amazone's Conditions of Use and Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+        </p>
+
+        <button className='register-button'>Create Your Amazone Account</button>
+      </div>
     </div>
   )
 };
