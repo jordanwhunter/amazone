3242b0053a8f626009c41976aba41818e9c28e84
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-03 19:28:19 -0500

    Created ForgotPassword component

---
+++ src/components/authentication/ForgotPassword.js
@@ -0,0 +1,13 @@
+// Dependencies
+import React from 'react'
+
+// Styles
+import '../../styles/authentication/ForgotPassword.css';
+
+export default function ForgotPassword() {
+  return (
+    <div>
+      Hello world. I'm the component you're directed to if you forgot your password
+    </div>
+  )
+};
