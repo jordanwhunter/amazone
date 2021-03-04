1b48bc21685ecdf07b3689c65075f35a314ac678
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-04 17:11:15 -0500

    Created Payment component

---
+++ src/components/processing/Payment.js
@@ -0,0 +1,17 @@
+// Dependencies
+import React from 'react';
+import Header from '../main/Header';
+
+// Styles
+import '../../styles/processing/Payment.css';
+
+export default function Payment() {
+  return (
+    <>
+      <Header />
+      <div>
+        Hello world. I'm the Payment component.
+      </div>
+    </>
+  )
+};
