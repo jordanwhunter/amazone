a3e1b4d6eb1f18ac25a87f0727adc4471d6f198d
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-05 14:27:22 -0500

    Launched firebase emulator for server - works

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,25 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+dotenv.config();
+
+const stripe = require("stripe")(`${process.env.REACT_APP_STRIPE_SECRET_KEY}`);
+
+// App config
+const app = express();
+
+// Middlewares
+app.use(cors({origin: true}));
+app.use(express.urlencoded({extended: true, useNewUrlParser: true}));
+app.use(express.json());
+
+// API routes
+app.get("/", (req, res) => res.status(200).send("Hello World"));
+
+// Listen command
+exports.api = functions.https.onRequest(app);
+
+// Example endpoint
+// http://localhost:5001/ama-zone/us-central1/api
