1f5cf944bc9b10158a1b97a3ce0f7dc3ad83e5b0
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-05 15:50:44 -0500

    Updated baseURL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: '...' //API (cloud function) URL goes here
+  baseURL: 'http://localhost:5001/ama-zone/us-central1/api' //API (cloud function) URL goes here
 });
 
 export default instance;
