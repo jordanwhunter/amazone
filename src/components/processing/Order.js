717cfc81608d7afb82971ca836464319f0d9c553
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-05 15:53:12 -0500

    Created new component for single order

---
+++ src/components/processing/Order.js
@@ -0,0 +1,13 @@
+// Dependencies
+import React from 'react';
+
+// Styles
+import '../../styles/processing/Order.css';
+
+export default function Order() {
+  return (
+    <div>
+      Hello world. I'm the Order component. 
+    </div>
+  )
+};
