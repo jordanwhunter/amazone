894357e52c6634ec2a19ab7aa02504985d512590
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-05 13:40:10 -0500

    Created Orders component and imported Stylesheet

---
+++ src/components/processing/Orders.js
@@ -0,0 +1,17 @@
+// Dependencies
+import React from 'react';
+import Header from '../main/Header';
+
+// Styles
+import '../../styles/processing/Orders.css';
+
+export default function Orders() {
+  return (
+    <>
+      <Header />
+      <div className='orders'>
+        Hello world. I am the Orders component.
+      </div>
+    </>
+  )
+};
