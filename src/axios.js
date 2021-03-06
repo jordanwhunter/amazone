5accdbff4f0b11eb774efc0b306cad5d87457d79
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-06 18:02:48 -0500

    Added cloud function API from deployed cloud server

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/ama-zone/us-central1/api' //API (cloud function) URL goes here
+  baseURL: 'https://us-central1-ama-zone.cloudfunctions.net/api' //API (cloud function) URL goes here
+  // http://localhost:5001/ama-zone/us-central1/api
 });
 
 export default instance;
