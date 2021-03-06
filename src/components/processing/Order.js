6063680178df1f363db81af764c8b7f7d2d56660
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-06 17:42:36 -0500

    Built component

--- src/components/processing/Order.js
+++ src/components/processing/Order.js
@@ -1,13 +1,47 @@
 // Dependencies
 import React from 'react';
+import CurrencyFormat from 'react-currency-format';
+import CheckoutProduct from '../cart/CheckoutProduct';
+import moment from 'moment';
 
 // Styles
 import '../../styles/processing/Order.css';
 
-export default function Order() {
+export default function Order({ order }) {
   return (
-    <div>
-      Hello world. I'm the Order component. 
+    <div className='order'>
+      <h2>Order</h2>
+      <p>
+        {
+          moment
+            .unix(order.data.created)
+            .format('MMMM Do YYYY, h:mma')
+        }
+      </p>
+      <p className='order-id'>
+        <small>{order.id}</small>
+      </p>
+      {order.data.cart?.map(item => (
+        <CheckoutProduct
+          id={item.id}
+          title={item.title}
+          image={item.image}
+          price={item.price}
+          rating={item.rating}
+          hideButton
+        />
+      ))}
+      <CurrencyFormat
+        // value acts as a render prop
+        renderText={(value) => (
+          <h3 className='order-total'>Order Total: {value}</h3>
+        )}
+        decimalScale={2}
+        value={order.data.amount / 100}
+        displayType={'text'}
+        thousandSeparator={true}
+        prefix={'$'}
+      />
     </div>
   )
 };
